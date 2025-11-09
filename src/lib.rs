86f520c0cb3433b2dcca39436a0353628117325b
helbadao <elbadaouihoussam4@gmail.com>
2025-11-09 12:44:04 +0100

    fixiing soomthiing in the arsng

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,3 @@ pub mod builtins;
 pub mod utils;
 pub mod lexer;
 pub mod parser;
-use crate::lexer::Lexertype;
--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,6 +1,9 @@
 use crate::lexer;
 
 pub fn parsing(lexer:Vec<lexer::tokenizer::Lexertype>) {
-    println!("Parsing function called with tokens: {:?}", Some(lexer[0].clone()));    
+    println!("Parsing function called with tokens: {:?}", lexer[0]);    
+    if let lexer::tokenizer::Lexertype::Word(word_content) = &lexer[0] {
+        println!("{}", word_content);
+    }
 
 }
