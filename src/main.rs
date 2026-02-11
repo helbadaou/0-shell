d61a3989276bfd74c4aac824fa0e0e382472bf24
Yousra <yousra.ch.etudiante@gmail.com>
2026-02-11 21:37:35 +0100

    rebasing the main

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ use crossterm::terminal::enable_raw_mode;
 use crossterm::{cursor, execute};
 use minishel::lexer;
 use minishel::parser::parser::parse_tokens;
-fn main() {
+use std::env;
+use std::io::{self, Write};
+fn main() -> std::io::Result<()> {
     let purple = "\x1b[35m";
     let cyan = "\x1b[36m";
     let reset = "\x1b[0m";
@@ -21,24 +23,39 @@ fn main() {
                  | |_| |          \__ \ | | | | |  __/ | | | | 
                   \___/           |___/ |_| |_|  \___| |_| |_| 
     "
-
     );
     println!("{}========f============================{}", purple, reset);
     println!("Type 'help' to see built-in commands\n");
     let mut line_buffer = String::new();
+    let mut input = String::new();
+
+    enable_raw_mode()?;
     loop {
         if line_buffer.is_empty() {
+            execute!(io::stdout(), cursor::MoveToColumn(0),)?;
+            match env::current_dir() {
+                Ok(path) => {
+                    execute!(io::stdout(), cursor::MoveToColumn(0),).unwrap();
+                    print!("{}{}{}", "\x1b[36m", path.display(), "\x1b[0m");
+                }
+                Err(_) => break,
+            }
             print!("$ ");
         } else {
+            execute!(io::stdout(), cursor::MoveToColumn(0),)?;
             print!("> ");
         }
         io::stdout().flush().unwrap();
+        input.clear();
 
-        let mut input = String::new();
-        if io::stdin().read_line(&mut input).is_err() {
+        // let mut input = String::new();
+        /*  if io::stdin().read_line(&mut input).is_err() {
             println!();
             break;
         }*/
+
+
+        
         // === READ ONE COMMAND ===
         loop {
             if let Event::Key(key) = event::read()? {
@@ -89,7 +106,7 @@ fn main() {
             }
         }
 
-        let input= input.trim();
+        let input = input.trim();
         if input.is_empty() {
             continue;
         }
@@ -97,10 +114,12 @@ fn main() {
             line_buffer.push('\n');
         }
         line_buffer.push_str(input.trim_end());
+        execute!(io::stdout(), cursor::MoveToColumn(0),)?;
 
         match lexer::tokenizer::tokenize(&line_buffer) {
             lexer::tokenizer::TokenizeResult::Success(tokens) => {
-                println!("Got tokens: {:?}", tokens);
+                /* execute!(io::stdout(), cursor::MoveToColumn(0),)?;
+                println!("Got tokens: {:?}", tokens); */
                 parse_tokens(tokens);
 
                 line_buffer.clear();
@@ -111,10 +130,12 @@ fn main() {
             }
 
             lexer::tokenizer::TokenizeResult::Error(err) => {
+                execute!(io::stdout(), cursor::MoveToColumn(0),)?;
                 eprintln!("{}", err);
                 line_buffer.clear();
             }
         }
     }
+    disable_raw_mode()?;
+    Ok(())
 }
-
