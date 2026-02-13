8b886422a807c0f1d21e13c547a148167bbc100a
Yousra <yousra.ch.etudiante@gmail.com>
2026-02-13 02:13:32 +0100

    fix some small errors after the merge

--- src/builtins/mod.rs
+++ src/builtins/mod.rs
@@ -3,4 +3,6 @@ pub mod cd;
 pub mod mkdir;
 pub mod cat;
 pub mod rm;
+pub mod cp;
+pub mod mv;
 pub mod ls;
--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,7 +1,7 @@
 use crate::lexer;
 use crossterm::terminal::disable_raw_mode;
-// use crossterm::{cursor, execute};
-// use std::io::{self};
+use crossterm::{cursor, execute};
+use std::io::{self};
 pub struct Command {
     pub name: String,
     pub args: Vec<String>,
