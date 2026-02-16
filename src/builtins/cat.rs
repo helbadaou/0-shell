5495a13df888f1ef791da9e543264951a5e03973
Yousra <yousra.ch.etudiante@gmail.com>
2026-02-16 04:53:29 +0000

    fix some errors for cat;

--- src/builtins/cat.rs
+++ src/builtins/cat.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{ self };
+use std::io::{ self, Read, Write };
 use std::path::Path;
 use crossterm::terminal::{ disable_raw_mode, enable_raw_mode };
 
@@ -7,35 +7,48 @@ pub fn cat(args: &[String]) {
     let mut stdout = io::stdout();
 
     if args.is_empty() {
-        // Disable raw mode to allow normal stdin reading
         let _ = disable_raw_mode();
         
         let mut stdin = io::stdin();
         match io::copy(&mut stdin, &mut stdout) {
             Err(e) => {
-                eprintln!("0-shell: cat: {}", e);
+                let error_msg = format!("0-shell: cat: {}\r\n", e);
+                let _ = io::stderr().write_all(error_msg.as_bytes());
             }
             Ok(_) => {}
         }
         
-        // Re-enable raw mode for the shell
         let _ = enable_raw_mode();
         return;
     }
     
     for filename in args {
         let path = Path::new(filename);
+        
+        // Check if it's a directory
+        if path.is_dir() {
+            let error_msg = format!("0-shell: cat: {}: Is a directory\r\n", filename);
+            let _ = io::stderr().write_all(error_msg.as_bytes());
+            continue;
+        }
+        
         match File::open(path) {
             Ok(mut file) => {
-                match io::copy(&mut file, &mut stdout) {
-                    Err(e) => {
-                        eprintln!("0-shell: cat: {}: {}", filename, e);
+                let mut contents = String::new();
+                match file.read_to_string(&mut contents) {
+                    Ok(_) => {
+                        let output = contents.replace("\n", "\r\n");
+                        let _ = io::stdout().write_all(output.as_bytes());
                     }
-                    Ok(_) => {}
+                    Err(e) => {
+                        let error_msg = format!("0-shell: cat: {}: {}\r\n", filename, e);
+                        let _ = io::stderr().write_all(error_msg.as_bytes());
+                    }
                 }
             }
             Err(e) => {
-                eprintln!("0-shell: cat: {}: {}", filename, e);
+                let error_msg = format!("0-shell: cat: {}: {}\r\n", filename, e);
+                let _ = io::stderr().write_all(error_msg.as_bytes());
             }
         }
     }
